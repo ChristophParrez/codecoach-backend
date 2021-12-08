7b6b5bde944d03c9335d54a3956d8835fde61d55
Glenn Verhaeghe <glennverhaeghe@gmail.com>
2021-12-08 13:27:35 +0100

    Started validation

--- src/main/java/be/codecoach/services/UserService.java
+++ src/main/java/be/codecoach/services/UserService.java
@@ -11,10 +11,12 @@ public class UserService {
 
     private final UserMapper userMapper;
     private final UserRepository userRepository;
+    private final MemberValidator memberValidator;
 
-    public UserService(UserMapper userMapper, UserRepository userRepository) {
+    public UserService(UserMapper userMapper, UserRepository userRepository, MemberValidator memberValidator) {
         this.userMapper = userMapper;
         this.userRepository = userRepository;
+        this.memberValidator = memberValidator;
     }
 
     public void registerUser(UserDto userDto) {
@@ -24,34 +26,6 @@ public class UserService {
     }
 
     private void assertUserInfoIsValid(UserDto userDto) {
-        assertDtoIsValid(userDto);
-        assertEmailIsValid(userDto.getEmail());
-        assertEmailIsUnique(userDto.getEmail()); // => userRepository.existsByEmail
-    }
-
-    private void assertDtoIsValid(UserDto userDto) {
-        MemberValidator memberValidator = new MemberValidator();
         memberValidator.validate(userDto);
-    }
-
-
-    private void assertEmailIsValid(String email) {
-        if (email == null) {
-            throw new IllegalArgumentException();
-        }
-
-        String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
-        java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern);
-        java.util.regex.Matcher m = p.matcher(email);
-
-        if(!m.matches()) {
-            throw new IllegalArgumentException();
-        }
-    }
-
-    private void assertEmailIsUnique(String email) {
-        if(userRepository.findByEmail(email) != null) {
-            throw new IllegalArgumentException();
-        }
     }
 }
--- src/main/java/be/codecoach/services/validators/MemberValidator.java
+++ src/main/java/be/codecoach/services/validators/MemberValidator.java
@@ -1,10 +1,48 @@
 package be.codecoach.services.validators;
 
 import be.codecoach.api.dtos.UserDto;
+import be.codecoach.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class MemberValidator{
 
-    public void validate(UserDto userDto) {
+    private final UserRepository userRepository;
 
+    @Autowired
+    public MemberValidator(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public void validate(UserDto userDto) {
+        assertDtoIsValid(userDto);
+        assertEmailIsValid(userDto.getEmail());
+        assertEmailIsUnique(userDto.getEmail());
+    }
+
+    private void assertDtoIsValid(UserDto userDto) {
+        // ToDo
+    }
+
+
+    private void assertEmailIsValid(String email) {
+        if (email == null) {
+            throw new IllegalArgumentException();
+        }
+
+        String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
+        java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern);
+        java.util.regex.Matcher m = p.matcher(email);
+
+        if(!m.matches()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void assertEmailIsUnique(String email) {
+        if(userRepository.findByEmail(email) != null) {
+            throw new IllegalArgumentException();
+        }
     }
 }
