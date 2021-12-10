9fd26e1b2b158e743faa6fcd9671c49340e7bdf5
Free Hoebeke <freehoebeke@gmail.com>
2021-12-10 08:29:26 +0100

    UserService: added encoding possibility for the password. Currently still coded out but it works.

--- src/main/java/be/codecoach/services/UserService.java
+++ src/main/java/be/codecoach/services/UserService.java
@@ -3,22 +3,22 @@ package be.codecoach.services;
 import be.codecoach.api.dtos.UserDto;
 import be.codecoach.domain.Role;
 import be.codecoach.domain.RoleEnum;
+import be.codecoach.domain.User;
 import be.codecoach.repositories.RoleRepository;
 import be.codecoach.repositories.UserRepository;
 import be.codecoach.security.authentication.user.api.Account;
 import be.codecoach.security.authentication.user.api.AccountService;
-import be.codecoach.security.authentication.user.api.CreateSecuredUserDto;
 import be.codecoach.services.mappers.UserMapper;
 import be.codecoach.services.validators.MemberValidator;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Service
+@Transactional //added by Free while doing tests with security
 public class UserService implements AccountService {
 
     private final UserMapper userMapper;
@@ -26,19 +26,24 @@ public class UserService implements AccountService {
     private final MemberValidator memberValidator;
     private final RoleRepository roleRepository;
 
+    private final PasswordEncoder passwordEncoder; //added by Free while doing tests with security
+
     @Autowired
-    public UserService(UserMapper userMapper, UserRepository userRepository, MemberValidator memberValidator, RoleRepository roleRepository) {
+    public UserService(UserMapper userMapper, UserRepository userRepository, MemberValidator memberValidator, RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
         this.userMapper = userMapper;
         this.userRepository = userRepository;
         this.memberValidator = memberValidator;
         this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;//added by Free while doing tests with security
     }
 
     public Account registerUser(UserDto userDto) {
         assertUserInfoIsValid(userDto);
         Role role = roleRepository.findByRole(RoleEnum.COACHEE);
-        //TODO: passwordEncoder.encode(createSecuredUserDto.getPassword()));
-        return userRepository.save(userMapper.toEntity(userDto, role));
+        User userToBeSaved = userMapper.toEntity(userDto, role);//added by Free while doing tests with security
+        //userToBeSaved.setPassword(passwordEncoder.encode(userDto.getPassword())); //remove comment to use encoding //added by Free while doing tests with security
+        return userRepository.save(userToBeSaved);
+        //added by Free while doing tests with security
     }
 
     private void assertUserInfoIsValid(UserDto userDto) {
