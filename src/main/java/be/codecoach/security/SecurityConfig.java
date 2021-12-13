48468847ab1b6274b7c51348006820e1379625ab
Glenn Verhaeghe <glennverhaeghe@gmail.com>
2021-12-13 17:26:46 +0100

    UserController test

--- src/main/java/be/codecoach/security/SecurityConfig.java
+++ src/main/java/be/codecoach/security/SecurityConfig.java
@@ -6,8 +6,10 @@ import be.codecoach.security.authentication.jwt.JwtAuthorizationFilter;
 import be.codecoach.security.authentication.jwt.JwtGenerator;
 import be.codecoach.security.authentication.user.SecuredUserService;
 import be.codecoach.security.authentication.user.api.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -16,7 +18,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.web.cors.CorsConfiguration;
@@ -33,6 +34,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final SecuredUserService securedUserService;
     private final AccountService accountService;
 
+    @Autowired
     public SecurityConfig(SecuredUserService securedUserService,
                           PasswordEncoder passwordEncoder,
                           JwtGenerator jwtGenerator,
@@ -43,7 +45,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         this.jwtGenerator = jwtGenerator;
         this.accountService = accountService;
     }
-
 
 
     @Override
--- src/test/java/be/codecoach/api/UserControllerTest.java
+++ src/test/java/be/codecoach/api/UserControllerTest.java
@@ -1,7 +1,58 @@
 package be.codecoach.api;
 
-import static org.junit.jupiter.api.Assertions.*;
+import be.codecoach.repositories.UserRepository;
+import be.codecoach.security.authentication.jwt.JwtGenerator;
+import be.codecoach.security.authentication.user.SecuredUserService;
+import be.codecoach.services.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@WebMvcTest
 class UserControllerTest {
+
+    @Autowired
+    WebApplicationContext wac;
+
+    @MockBean
+    UserRepository userRepository;
+
+    @MockBean
+    UserService userService;
+
+    @MockBean
+    SecuredUserService securedUserService;
+
+    @MockBean
+    PasswordEncoder passwordEncoder;
+
+    @MockBean
+    JwtGenerator jwtGenerator;
+
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(wac).apply(springSecurity()).build();
+    }
+
+    @Test
+    void getUser() throws Exception {
+        mockMvc.perform(get("/users/123456")).andExpect(status().isOk());
+    }
+
 
 }
