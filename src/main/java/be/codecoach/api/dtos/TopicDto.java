540f26c6dcd822d7a7ea526dc0df8cd18a68bccd
Glenn Verhaeghe <glennverhaeghe@gmail.com>
2021-12-14 14:10:11 +0100

    Update topicDto

--- src/main/java/be/codecoach/api/dtos/TopicDto.java
+++ src/main/java/be/codecoach/api/dtos/TopicDto.java
@@ -1,19 +1,11 @@
 package be.codecoach.api.dtos;
 
-import javax.persistence.Column;
-
 public class TopicDto {
 
-    private final String topicId;
     private final String name;
 
-    private TopicDto(String topicId, String name) {
-        this.topicId = topicId;
+    private TopicDto(String name) {
         this.name = name;
-    }
-
-    public String getTopicId() {
-        return topicId;
     }
 
     public String getName() {
@@ -21,7 +13,6 @@ public class TopicDto {
     }
 
     public static final class Builder {
-        private String topic_id;
         private String name;
 
         private Builder() {
@@ -31,18 +22,13 @@ public class TopicDto {
             return new Builder();
         }
 
-        public Builder withTopic_id(String topic_id) {
-            this.topic_id = topic_id;
-            return this;
-        }
-
         public Builder withName(String name) {
             this.name = name;
             return this;
         }
 
         public TopicDto build() {
-            return new TopicDto(topic_id, name);
+            return new TopicDto(name);
         }
     }
 }
