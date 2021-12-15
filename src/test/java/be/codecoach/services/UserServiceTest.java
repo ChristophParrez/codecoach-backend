fa042459fa9efa790b10e7aa1dfaaabd3e00b772
Mert1980 <mertdemirok80@gmail.com>
2021-12-15 09:13:37 +0100

    add test for registering user in UserServiceTest

---
+++ src/test/java/be/codecoach/services/UserServiceTest.java
@@ -0,0 +1,119 @@
+package be.codecoach.services;
+
+import be.codecoach.api.dtos.RoleDto;
+import be.codecoach.api.dtos.UserDto;
+import be.codecoach.builder.UserTestBuilder;
+import be.codecoach.domain.Role;
+import be.codecoach.domain.RoleEnum;
+import be.codecoach.domain.User;
+import be.codecoach.repositories.CoachingTopicRepository;
+import be.codecoach.repositories.RoleRepository;
+import be.codecoach.repositories.UserRepository;
+import be.codecoach.services.mappers.CoachingTopicMapper;
+import be.codecoach.services.mappers.RoleMapper;
+import be.codecoach.services.mappers.UserMapper;
+import be.codecoach.services.validators.MemberValidator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+
+class UserServiceTest {
+
+    private UserService userService;
+    private UserMapper userMapperMock;
+    private RoleMapper roleMapperMock;
+    private UserRepository userRepositoryMock;
+    private MemberValidator memberValidatorMock;
+    private RoleRepository roleRepositoryMock;
+    private PasswordEncoder passwordEncoderMock;
+    private CoachingTopicMapper coachingTopicMapperMock;
+    private CoachingTopicRepository coachingTopicRepositoryMock;
+    private TopicService topicServiceMock;
+    private CoachInformationService coachInformationServiceMock;
+
+
+    @BeforeEach
+    void setUp() {
+        userMapperMock = Mockito.mock(UserMapper.class);
+        userRepositoryMock = Mockito.mock(UserRepository.class);
+        memberValidatorMock = Mockito.mock(MemberValidator.class);
+        roleRepositoryMock = Mockito.mock(RoleRepository.class);
+        passwordEncoderMock = Mockito.mock(PasswordEncoder.class);
+        coachingTopicMapperMock = Mockito.mock(CoachingTopicMapper.class);
+        coachingTopicRepositoryMock = Mockito.mock(CoachingTopicRepository.class);
+        topicServiceMock = Mockito.mock(TopicService.class);
+        coachInformationServiceMock = Mockito.mock(CoachInformationService.class);
+        roleMapperMock = Mockito.mock(RoleMapper.class);
+        userService = new UserService(userMapperMock, roleMapperMock, userRepositoryMock, memberValidatorMock,
+                roleRepositoryMock, passwordEncoderMock, coachingTopicMapperMock, coachingTopicRepositoryMock,
+                topicServiceMock, coachInformationServiceMock);
+    }
+
+    @Test
+    void registerUser() {
+        // GIVEN
+        User user = UserTestBuilder.anUser().build();
+        Role role = new Role(RoleEnum.COACHEE);
+        UserDto userDto = UserDto.Builder.anUserDto().build();
+
+        // WHEN
+        doNothing().when(memberValidatorMock).validate(userDto);
+
+        when(roleRepositoryMock.findByRole(RoleEnum.COACHEE)).thenReturn(role);
+        when(userMapperMock.toEntity(userDto, role)).thenReturn(user);
+        when(passwordEncoderMock.encode(any())).thenReturn(user.getPassword());
+
+        userService.registerUser(userDto);
+
+        // THEN
+        Mockito.verify(userRepositoryMock).save(user);
+    }
+
+    @Test
+    void findByEmail() {
+    }
+
+    @Test
+    void createAccount() {
+    }
+
+    @Test
+    void existsByEmail() {
+    }
+
+    @Test
+    void getUser() {
+    }
+
+    @Test
+    void getCoacheeProfileDto() {
+    }
+
+    @Test
+    void getCoachProfileDto() {
+    }
+
+    @Test
+    void becomeCoach() {
+    }
+
+    @Test
+    void updateUser() {
+    }
+
+    @Test
+    void updateCoach() {
+    }
+
+    @Test
+    void addTopic() {
+    }
+}
