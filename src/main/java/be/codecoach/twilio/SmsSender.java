9acc2690b589feea966760964e77b420ab099bbc
Glenn Verhaeghe <glennverhaeghe@gmail.com>
2021-12-21 13:32:34 +0100

    Deleted unused service

--- src/main/java/be/codecoach/twilio/SmsSender.java
+++ src/main/java/be/codecoach/twilio/SmsSender.java
@@ -3,8 +3,6 @@ package be.codecoach.twilio;
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 
 @Service
