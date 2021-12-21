43a0f62ef6e295ee3ca92a5f933408af0564c421
Glenn Verhaeghe <glennverhaeghe@gmail.com>
2021-12-21 13:33:43 +0100

    Deleted commented out code

--- src/main/java/be/codecoach/security/authentication/user/SecuredUserController.java
+++ src/main/java/be/codecoach/security/authentication/user/SecuredUserController.java
@@ -17,19 +17,6 @@ public class SecuredUserController {
     public SecuredUserController(SecuredUserService securedUserService) {
         this.securedUserService = securedUserService;
     }
-/*
-    @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping(consumes = "application/json;charset=UTF-8", produces = "application/json;charset=UTF-8")
-    public SecuredUserDto createUser(@RequestBody CreateSecuredUserDto createSecuredUserDto) {
-        if (!isEmailValid(createSecuredUserDto.getEmail())) {
-            throw new IllegalStateException("Email is not valid !");
-        }
-        if (!isPasswordValid(createSecuredUserDto.getPassword())) {
-            throw new IllegalStateException("Password needs te be 8 characters : --> 1 capital, 1 lowercase and 1 one number ");
-        }
-        LOGGER.info("User was added");
-        return securedUserService.registerAccount(createSecuredUserDto);
-    }*/
 
     @PreAuthorize("isAnonymous()")
     @PostMapping(consumes = "application/json;charset=UTF-8", produces = "application/json;charset=UTF-8", path = "/validate")
@@ -57,22 +44,6 @@ public class SecuredUserController {
         }
         return securedUserService.performPasswordChange(changeRequest);
     }
-
-    /*@ExceptionHandler(IllegalStateException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public void invalidFieldsException(IllegalStateException ex, HttpServletResponse response) throws IOException {
-        LOGGER.info(ex.getMessage());
-        response.sendError(400, ex.getMessage());
-    }*/
-/*
-    boolean isEmailValid(String email) {
-        if (email == null) return false;
-
-        String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
-        java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern);
-        java.util.regex.Matcher m = p.matcher(email);
-        return m.matches();
-    }*/
 
     boolean isPasswordValid(String password) {
         if (password == null) return false;
