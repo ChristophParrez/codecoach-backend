19f1e0ce150627db10fd8523aa90f3ed1c93ac57
Glenn Verhaeghe <glennverhaeghe@gmail.com>
2021-12-23 09:50:18 +0100

    Update data

--- data/input.sql
+++ data/input.sql
@@ -63,7 +63,7 @@ INSERT INTO coaching_topics (coaching_info_id, topic, experience, coaching_topic
 
 -- Glenn
 INSERT INTO coach_info (introduction, coach_info_id, availability, coach_xp) values (null, '2002', null, 0);
-INSERT INTO users (user_id, company_name, email, first_name, last_name, password, picture, coach_info, telephone_number) values ('1002', 'eGov', 'glenn@mail.be', 'Glenn', 'Verhaeghe', '$2a$10$6nCHRHSI92.guttxstf/XO6S7Xe.SV5n9hpOw1HerNOpwb8/OyFm2', 'https://randomuser.me/api/portraits/men/12.jpg', '2002', '+32472491759');
+INSERT INTO users (user_id, company_name, email, first_name, last_name, password, picture, coach_info, telephone_number) values ('1002', 'eGov', 'glenn@mail.be', 'Michel', 'Drets', '$2a$10$6nCHRHSI92.guttxstf/XO6S7Xe.SV5n9hpOw1HerNOpwb8/OyFm2', 'https://randomuser.me/api/portraits/men/12.jpg', '2002', '+32472491759');
 INSERT INTO users_roles (user_id, role_id) values ('1002', '0');
 INSERT INTO users_roles (user_id, role_id) values ('1002', '1');
 INSERT INTO users_roles (user_id, role_id) values ('1002', '2');
